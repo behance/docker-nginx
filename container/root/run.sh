af68b0075e4e4e7d5462c3d7fff810db51a4a148
Israel Derdik <derdik@adobe.com>
2015-08-14 11:25:23 -0400

    added placeholder to access_log format for app name

--- container/root/run.sh
+++ container/root/run.sh
@@ -7,7 +7,7 @@ sed -i "s/worker_processes [0-9]\+/worker_processes $(nproc)/" /etc/nginx/nginx.
 sed -i "s/worker_connections [0-9]\+/worker_connections 1024/" /etc/nginx/nginx.conf
 
 # Ensure nginx is configured to write logs to STDOUT
-sed -i "s/access_log [a-z\/\.\;]\+/access_log \/dev\/stdout;/" /etc/nginx/nginx.conf
+sed -i "s/access_log [a-z\/\.\;]\+/            log_format main \'\$remote_addr - \$remote_user [\$time_local] \"\$request\" \$status \$bytes_sent \"\$http_referer\" \"\$http_user_agent\" LOG_APP_NAME\';\n        access_log \/dev\/stdout main;\n/" /etc/nginx/nginx.conf
 sed -i "s/error_log [a-z\/\.\ \;]\+/error_log \/dev\/stdout info;/" /etc/nginx/nginx.conf
 
 echo "Starting Nginx (foreground)"
