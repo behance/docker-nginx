34e6b31cbc281d86c6b6eaa81cb319da398188c4
Bryan Latten <latten@adobe.com>
2017-02-24 15:26:53 -0500

    Nginx: running master process back at root
    Removing /dev/stdout hacks in order to make this 
    work. Unnecessary precaution for unconnected master
    process, further mitigated with user namespaces 
    going forward

--- container/root/etc/cont-init.d/01-stdout.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bash
-
-# STDOUT happens to be a very special "file" that is has Docker magic applied to it
-# Unfortunately, permissions are flaky between builds, sometimes causing unprivileged users
-# to hit issues when attempting to write to STDOUT
-
-# Hack: add others read/write back in at runtime :/
-chmod o+rw /dev/stdout
